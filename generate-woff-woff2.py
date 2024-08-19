61bcea10f1ab5fb577059c80b3fdfaf2e91e5176
Cristiano Sobral <cssobral2013@gmail.com>
2024-08-19 00:27:49 -0300

    Update

---
+++ generate-woff-woff2.py
@@ -0,0 +1,372 @@
+from fontTools.ttLib import TTFont
+
+'''Generate WOFF from TTF or OTF font.'''
+
+srcPath  = './ttf/BrisaSans-Thin.ttf'
+woffPath = './web/BrisaSans-Thin.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)
+
+srcPath  = './ttf/BrisaSans-ThinItalic.ttf'
+woffPath = './web/BrisaSans-ThinItalic.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)
+
+srcPath  = './ttf/BrisaSans-Light.ttf'
+woffPath = './web/BrisaSans-Light.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)
+
+srcPath  = './ttf/BrisaSans-LightItalic.ttf'
+woffPath = './web/BrisaSans-LightItalic.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)
+    
+srcPath  = './ttf/BrisaSans-Regular.ttf'
+woffPath = './web/BrisaSans-Regular.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+    
+srcPath  = './ttf/BrisaSans-Italic.ttf'
+woffPath = './web/BrisaSans-Italic.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+    
+srcPath  = './ttf/BrisaSans-Medium.ttf'
+woffPath = './web/BrisaSans-Medium.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+    
+srcPath  = './ttf/BrisaSans-MediumItalic.ttf'
+woffPath = './web/BrisaSans-MediumItalic.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+    
+srcPath  = './ttf/BrisaSans-Bold.ttf'
+woffPath = './web/BrisaSans-Bold.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+    
+srcPath  = './ttf/BrisaSans-BoldItalic.ttf'
+woffPath = './web/BrisaSans-BoldItalic.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+    
+srcPath  = './ttf/BrisaSans-Black.ttf'
+woffPath = './web/BrisaSans-Black.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+    
+srcPath  = './ttf/BrisaSans-BlackItalic.ttf'
+woffPath = './web/BrisaSans-BlackItalic.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+    
+srcPath  = './ttf/BrisaSans-ThinCd.ttf'
+woffPath = './web/BrisaSans-ThinCd.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)
+
+srcPath  = './ttf/BrisaSans-ThinCdItalic.ttf'
+woffPath = './web/BrisaSans-ThinCdItalic.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)
+
+srcPath  = './ttf/BrisaSans-LightCd.ttf'
+woffPath = './web/BrisaSans-LightCd.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)
+
+srcPath  = './ttf/BrisaSans-LightCdItalic.ttf'
+woffPath = './web/BrisaSans-LightCdItalic.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)
+    
+srcPath  = './ttf/BrisaSans-RegularCd.ttf'
+woffPath = './web/BrisaSans-RegularCd.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+    
+srcPath  = './ttf/BrisaSans-CdItalic.ttf'
+woffPath = './web/BrisaSans-CdItalic.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+    
+srcPath  = './ttf/BrisaSans-MediumCd.ttf'
+woffPath = './web/BrisaSans-MediumCd.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+    
+srcPath  = './ttf/BrisaSans-MediumCdItalic.ttf'
+woffPath = './web/BrisaSans-MediumCdItalic.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+    
+srcPath  = './ttf/BrisaSans-BoldCd.ttf'
+woffPath = './web/BrisaSans-BoldCd.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+    
+srcPath  = './ttf/BrisaSans-BoldCdItalic.ttf'
+woffPath = './web/BrisaSans-BoldCdItalic.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+    
+srcPath  = './ttf/BrisaSans-BlackCd.ttf'
+woffPath = './web/BrisaSans-BlackCd.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+    
+srcPath  = './ttf/BrisaSans-BlackCdItalic.ttf'
+woffPath = './web/BrisaSans-BlackCdItalic.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)  
+
+
+'''Generate WOFF2 from TTF or OTF font.'''
+
+srcPath  = './ttf/BrisaSans-Thin.ttf'
+woff2Path = './web/BrisaSans-Thin.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)
+
+srcPath  = './ttf/BrisaSans-ThinItalic.ttf'
+woff2Path = './web/BrisaSans-ThinItalic.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)
+
+srcPath  = './ttf/BrisaSans-Light.ttf'
+woff2Path = './web/BrisaSans-Light.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)
+
+srcPath  = './ttf/BrisaSans-LightItalic.ttf'
+woff2Path = './web/BrisaSans-LightItalic.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)
+    
+srcPath  = './ttf/BrisaSans-Regular.ttf'
+woff2Path = './web/BrisaSans-Regular.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)    
+
+    
+srcPath  = './ttf/BrisaSans-Italic.ttf'
+woff2Path = './web/BrisaSans-Italic.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)    
+
+    
+srcPath  = './ttf/BrisaSans-Medium.ttf'
+woff2Path = './web/BrisaSans-Medium.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)    
+
+    
+srcPath  = './ttf/BrisaSans-MediumItalic.ttf'
+woff2Path = './web/BrisaSans-MediumItalic.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)    
+
+    
+srcPath  = './ttf/BrisaSans-Bold.ttf'
+woff2Path = './web/BrisaSans-Bold.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)    
+
+    
+srcPath  = './ttf/BrisaSans-BoldItalic.ttf'
+woff2Path = './web/BrisaSans-BoldItalic.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)    
+
+    
+srcPath  = './ttf/BrisaSans-Black.ttf'
+woff2Path = './web/BrisaSans-Black.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)    
+
+    
+srcPath  = './ttf/BrisaSans-BlackItalic.ttf'
+woff2Path = './web/BrisaSans-BlackItalic.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)    
+
+    
+srcPath  = './ttf/BrisaSans-ThinCd.ttf'
+woff2Path = './web/BrisaSans-ThinCd.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)
+
+srcPath  = './ttf/BrisaSans-ThinCdItalic.ttf'
+woff2Path = './web/BrisaSans-ThinCdItalic.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)
+
+srcPath  = './ttf/BrisaSans-LightCd.ttf'
+woff2Path = './web/BrisaSans-LightCd.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)
+
+srcPath  = './ttf/BrisaSans-LightCdItalic.ttf'
+woff2Path = './web/BrisaSans-LightCdItalic.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)
+    
+srcPath  = './ttf/BrisaSans-RegularCd.ttf'
+woff2Path = './web/BrisaSans-RegularCd.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)    
+
+    
+srcPath  = './ttf/BrisaSans-CdItalic.ttf'
+woff2Path = './web/BrisaSans-CdItalic.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)    
+
+    
+srcPath  = './ttf/BrisaSans-MediumCd.ttf'
+woff2Path = './web/BrisaSans-MediumCd.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)    
+
+    
+srcPath  = './ttf/BrisaSans-MediumCdItalic.ttf'
+woff2Path = './web/BrisaSans-MediumCdItalic.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)    
+
+    
+srcPath  = './ttf/BrisaSans-BoldCd.ttf'
+woff2Path = './web/BrisaSans-BoldCd.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)    
+
+    
+srcPath  = './ttf/BrisaSans-BoldCdItalic.ttf'
+woff2Path = './web/BrisaSans-BoldCdItalic.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)    
+
+    
+srcPath  = './ttf/BrisaSans-BlackCd.ttf'
+woff2Path = './web/BrisaSans-BlackCd.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)    
+
+    
+srcPath  = './ttf/BrisaSans-BlackCdItalic.ttf'
+woff2Path = './web/BrisaSans-BlackCdItalic.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path) 
